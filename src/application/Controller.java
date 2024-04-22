7a0195175de8d5bb5fb1e2c515cfc604afec45d4
YElfeky2004 <167637260+YElfeky2004@users.noreply.github.com>
2024-04-22 17:34:39 +0200

    Update Controller.java

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -56,14 +56,7 @@ public class Controller {
 	    }
 	}
 
-	
-	public void switchToScene1(ActionEvent event) throws IOException{
-		Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
+
 }
 
 
